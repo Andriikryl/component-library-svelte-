87b1ab74e2b6de4c92855178fa921ac3d9c2477c
Andriikryl <andreyk557@gmail.com>
2024-02-09 18:17:55 +0200

    add styles to the componentsGroup component

--- src/components/ComponentsGroup.svelte
+++ src/components/ComponentsGroup.svelte
@@ -3,12 +3,26 @@
   import AccordionItem from "./AccordionItem.svelte";
 </script>
 
-<div>
-  <h1>components librery</h1>
-  <Accordion collapse>
-    <AccordionItem />
-    <AccordionItem />
-    <AccordionItem />
-    <AccordionItem />
-  </Accordion>
-</div>
+<section class="content">
+  <div class="container">
+    <h1>components librery</h1>
+    <div class="content__box">
+      <Accordion collapse>
+        <AccordionItem />
+        <AccordionItem />
+        <AccordionItem />
+        <AccordionItem />
+      </Accordion>
+    </div>
+  </div>
+</section>
+
+<style>
+  .content {
+    margin-block: 20px;
+  }
+  .content__box {
+    margin-block: 50px;
+    max-width: 500px;
+  }
+</style>
