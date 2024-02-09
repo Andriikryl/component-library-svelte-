f4474ed41fe4d421a83e691c008e603832df1d5f
Andriikryl <andreyk557@gmail.com>
2024-02-09 19:01:22 +0200

    add items array

--- src/components/ComponentsGroup.svelte
+++ src/components/ComponentsGroup.svelte
@@ -1,17 +1,42 @@
 <script>
   import Accordion from "./Accordion.svelte";
   import AccordionItem from "./AccordionItem.svelte";
+
+  const items = [
+    {
+      title: "Item 1",
+      content:
+        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, eveniet magni. Recusandae, nihil voluptatem labore tempora ipsam eos corrupti doloremque aliquam est veritatis quia suscipit minus in incidunt velit at?",
+    },
+    {
+      title: "Item 2",
+      content:
+        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, eveniet magni. Recusandae, nihil voluptatem labore tempora ipsam eos corrupti doloremque aliquam est veritatis quia suscipit minus in incidunt velit at?",
+    },
+    {
+      title: "Item 3",
+      content:
+        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, eveniet magni. Recusandae, nihil voluptatem labore tempora ipsam eos corrupti doloremque aliquam est veritatis quia suscipit minus in incidunt velit at?",
+    },
+    {
+      title: "Item 4",
+      content:
+        "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, eveniet magni. Recusandae, nihil voluptatem labore tempora ipsam eos corrupti doloremque aliquam est veritatis quia suscipit minus in incidunt velit at?",
+    },
+  ];
 </script>
 
 <section class="content">
   <div class="container">
     <h1>components librery</h1>
     <div class="content__box">
-      <Accordion collapse>
-        <AccordionItem />
-        <AccordionItem />
-        <AccordionItem />
-        <AccordionItem />
+      <Accordion collapse --accordion-width="60ch">
+        {#each items as item, i}
+          <AccordionItem open={i === 0}>
+            <svelte:fragment slot="title">{item.title}</svelte:fragment>
+            <svelte:fragment slot="content">{item.content}</svelte:fragment>
+          </AccordionItem>
+        {/each}
       </Accordion>
     </div>
   </div>
