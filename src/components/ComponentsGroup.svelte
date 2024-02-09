64879fa05d76367a02857d6e48805370fd7e7c74
Andriikryl <andreyk557@gmail.com>
2024-02-09 18:16:04 +0200

    add ComponentsGroup component

---
+++ src/components/ComponentsGroup.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Accordion from "./Accordion.svelte";
+  import AccordionItem from "./AccordionItem.svelte";
+</script>
+
+<div>
+  <h1>components librery</h1>
+  <Accordion collapse>
+    <AccordionItem />
+    <AccordionItem />
+    <AccordionItem />
+    <AccordionItem />
+  </Accordion>
+</div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
-export { default as Accordion } from './accordion.svelte'
-export { default as AccordionItem } from './accordion-item.svelte'
+export { default as Accordion } from './Accordion.svelte'
+export { default as AccordionItem } from './AccordionItem.svelte'
