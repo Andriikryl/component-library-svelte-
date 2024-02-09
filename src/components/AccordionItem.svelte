7d7c5914bf83131f7b92fac2333ecc1a0e7ba4e2
Andriikryl <andreyk557@gmail.com>
2024-02-09 19:00:38 +0200

    add function context to the accordionItem component

--- src/components/AccordionItem.svelte
+++ src/components/AccordionItem.svelte
@@ -1,34 +1,68 @@
 <script lang="ts">
-  export let open;
+  import { slide } from "svelte/transition";
+  import { getAccordionOptions } from "./context";
+
+  export let open = false;
+  const componentId = crypto.randomUUID();
+  const { collapse, activeComponentId } = getAccordionOptions();
+
+  function setActive() {
+    $activeComponentId = componentId;
+  }
 
   function toggleOpen() {
     open = !open;
   }
+
+  function handleClick() {
+    collapse ? setActive() : toggleOpen();
+  }
+
+  $: open && collapse && setActive();
+  $: isActive = $activeComponentId === componentId;
+  $: isOpen = collapse ? isActive : open;
 </script>
 
 <div class="accordion-item">
-  <button on:click={toggleOpen} class="accordion-toggle">
+  <button
+    on:click={handleClick}
+    class="accordion-toggle"
+    aria-expanded={isOpen}
+    aria-controls="accordion-{componentId}"
+  >
     <div class="accordion-title">
       <slot name="title" />
     </div>
 
-    <div class="accordion-caret">👉️</div>
+    <div class="accordion-caret" class:open={isOpen}></div>
   </button>
 
-  {#if open}
-    <div class="accordion-content">
+  {#if isOpen}
+    <div
+      transition:slide|local
+      class="accordion-content"
+      role="region"
+      aria-hidden={!isOpen}
+      aria-labelledby="accordion-{componentId}"
+    >
       <slot name="content" />
     </div>
   {/if}
 </div>
 
 <style>
+  .accordion-item {
+    background-color: #ffffff;
+    border: 1px solid #000000;
+    border-radius: var(--accordion-radius, 4px);
+  }
   .accordion-toggle {
     width: 100%;
     display: flex;
+    align-items: center;
     justify-content: space-between;
     padding: var(--accordion-padding, 1rem);
-    color: var(--accordion-color, inherit);
+    color: var(--accordion-color, #14213d);
     font: inherit;
     font-weight: 600;
     border: none;
@@ -37,11 +71,42 @@
     border-radius: var(--accordion-radius, 4px);
     transition: background-color 0.1s ease;
   }
-  .accordion-toggle:hover {
-    background-color: var(--accordion-hover, hsl(220 20% 20%));
+
+  .accordion-caret {
+    width: 20px;
+    height: 2px;
+    background-color: #000000;
+    transition: rotate 0.3s ease;
+    position: relative;
+  }
+  .accordion-caret::after {
+    content: "";
+    display: block;
+    background-color: #000000;
+    width: 10px;
+    height: 2px;
+    position: absolute;
+    right: 2px;
+    top: -3px;
+    transform: rotate(20deg);
+  }
+  .accordion-caret::before {
+    content: "";
+    display: block;
+    background-color: #000000;
+    width: 10px;
+    height: 2px;
+    position: absolute;
+    right: 2px;
+    top: 3px;
+    transform: rotate(-20deg);
+  }
+  .accordion-content {
+    color: #14213d;
+    padding: var(--accordion-content-padding, 1rem);
   }
 
-  .accordion-content {
-    padding: var(--accordion-content-padding, 1rem);
+  .open {
+    rotate: 90deg;
   }
 </style>
